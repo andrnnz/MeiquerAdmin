61ce0c0e8d6682bfc810d3b9d7951e4ebc03fe73
robertocs21 <robertocastro1997@hotmail.com>
2023-06-04 18:40:50 -0600

    server

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-
+// server/index.js
 const express = require("express");
-const BodyParser = require("body-parser");
+var path = require('path');
+const bodyParser = require('body-parser');
+const fs = require("fs");
+const multer = require("multer");
+const mimeTypes = require("mime-types");
 
+
+const storage = multer.diskStorage({
+    destination: 'archivos/',
+    filename:function(req,file,cb){
+        cb("",file.originalname);
+    }
+});
+
+const PORT = process.env.PORT || 3002;
 const app = express();
-
-app.set("view engine", "ejs");
-
-app.get("/", function(req, res){
-    res.send("Hello");
+app.use(bodyParser.json());
+const upload = multer({
+    storage: storage
 });
+// obtiene la ruta del directorio publico donde se encuentran los elementos estaticos (css, js).
+var publicPath = path.resolve(__dirname, 'public'); //path.join(__dirname, 'public'); también puede ser una opción
 
-app.listen("3000", function(){
-    console.log("Server running on port 3000");
+// Para que los archivos estaticos queden disponibles.
+app.use(express.static(publicPath));
+app.get("/", (req, res) => {
+    console.log(__dirname);
+  res.sendFile(__dirname + "/public/subir.html");
 });
+app.post("/files",upload.single('archivos') ,(req, res) => {
+   
+  });
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
+  
